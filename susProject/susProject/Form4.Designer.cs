4a4b72838bad1dcc0e40cfc4ac08abf4639662a1
Sylu <mortisflos@gmail.com>
2022-09-25 02:18:17 +0300

    add dinamically buttons

--- susProject/susProject/Form4.Designer.cs
+++ susProject/susProject/Form4.Designer.cs
@@ -42,6 +42,7 @@
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.textBox7 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
@@ -154,12 +155,30 @@
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(212, 26);
             this.textBox7.TabIndex = 15;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            this.textBox7.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox7_KeyPress);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox1.Location = new System.Drawing.Point(727, 353);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 16;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1165, 683);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox7);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.textBox6);
@@ -197,5 +216,6 @@
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- susProject/susProject/Form4.cs
+++ susProject/susProject/Form4.cs
@@ -26,5 +26,43 @@ namespace susProject
         {
 
         }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            string count = textBox1.Text;
+
+            Button setMaterial = new Button();
+            setMaterial.Text = count;
+            this.Controls.Add(setMaterial);
+
+        }
+
+        private void textBox7_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string count = comboBox1.SelectedItem.ToString();
+            int count1 = int.Parse(count);
+
+            int top = 50;
+            int left = 100;
+
+            for (int i = 0; i < count1; i++)
+            {
+                Button button = new Button();
+                button.Left = left;
+                button.Top = top;
+                this.Controls.Add(button);
+                Console.WriteLine(1);
+                top += button.Height + 2;
+            }
+        }
     }
 }
