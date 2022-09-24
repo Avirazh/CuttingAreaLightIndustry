f7397d08b985b6928b547ed836a6860081bdd02d
Avirazh <murmin03@gmail.com>
2022-09-24 19:00:11 +0300

    Result model added

---
+++ susProject/susProject/Model/ResultTable/ResultGroup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace susProject.Model
+{
+    public class ResultGroup
+    {
+        public string Name { get; set; }
+        public List<ResultLine> resultLines { get; set; }
+        /// <summary>
+        /// Фактическое количество рабочих
+        /// </summary>
+        public int ActualWorkersQuantity { get; set; }
+        /// <summary>
+        /// Количество оборудования К.об
+        /// </summary>
+        public int EquipmentAmount { get; set; }
+        /// <summary>
+        /// Коэффициент использования площади К.пл
+        /// </summary>
+        public double AreaUsageTotalRate { get; set; }
+        /// <summary>
+        /// Площадь участка S, м^2
+        /// </summary>
+        public double LandArea { get; set; }
+    }
+}
---
+++ susProject/susProject/Model/ResultTable/ResultLine.cs
@@ -0,0 +1,14 @@
+﻿namespace susProject.Model
+{
+    public class ResultLine
+    {
+        /// <summary>
+        /// Наименование процесса
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Расчетное количество рабочих
+        /// </summary>
+        public double EstWorkersQuantity { get; set; }
+    }
+}
