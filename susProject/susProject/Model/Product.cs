f55344c2318d866268772a0ef09d31adf57a3a99
Avirazh <murmin03@gmail.com>
2022-09-24 18:39:41 +0300

    models completed

--- susProject/susProject/Model/Product.cs
+++ susProject/susProject/Model/Product.cs
@@ -4,9 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace susProject.Logic
+namespace susProject.Model
 {
-    internal class Product
+    public class Product
     {
+        public string Name { get; set; }
+
+        public List<Material> Materials { get; set; }
+
+        /// <summary>
+        /// Выпуск изделий М, ед./см 
+        /// </summary>
+        public double ProdReleaseAmount { get; set; }
+        /// <summary>
+        /// Время на проверку качества сборки и комплектования дет.кроя (tкомп)
+        /// </summary>
+        public double QualityCheckTime { get; set; }
+        /// <summary>
+        /// Время на нумерацию дет.кроя (tнум)
+        /// </summary>
+        public double NumerationTime { get; set; }
+        /// <summary>
+        /// Время на печатание тов. ярлыков (tп.я)
+        /// </summary>
+        public double LabelPrintTime { get; set; }
+        /// <summary>
+        /// Время на дублирование деталей (tпр)
+        /// </summary>
+        public double DetailsDuplicTime { get; set; }
+
+        public List<Material> AddMaterial(Material material)
+        {
+            Materials.Add(material);
+            return Materials;
+        }
     }
 }
